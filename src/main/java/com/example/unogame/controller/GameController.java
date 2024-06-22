e30516fe92d6146fa8a5ad5270dbf28ac979375f
Manuel Parra <parra.manuel@correounivalle.edu.co>
2024-06-22 16:53:21 -0500

    Deck button implemented

--- src/main/java/com/example/unogame/controller/GameController.java
+++ src/main/java/com/example/unogame/controller/GameController.java
@@ -3,23 +3,27 @@ package com.example.unogame.controller;
 import com.example.unogame.model.url_cards.FilePathEnum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.net.URL;
-import java.util.Objects;
+import javafx.scene.layout.*;
 
 public class GameController {
 
     @FXML
-    Button deckButton;
+    Button deckButton, unoButton;
+
+    @FXML
+    GridPane playerCards, botCards;
 
     @FXML
     public void initialize() {
-        /*Image deckImage = new Image(String.valueOf(getClass().getResourceAsStream("images/uno-cards/deck_of_cards.png")));
+        Image deckImage = new Image(String.valueOf(getClass().getResource("/com/example/unogame/images/uno-button.png")), 150, 100, true, false);
         ImageView deckImageView = new ImageView(deckImage);
-        deckButton.setGraphic(deckImageView);*/
+        //unoButton.setGraphic(deckImageView);
+        //unoButton.setPadding(new Insets(-10, -10, -10, -10));
+        unoButton.setBackground(new Background(new BackgroundImage(deckImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
     }
 
     public void onHandleButtonDeck(ActionEvent actionEvent) {
